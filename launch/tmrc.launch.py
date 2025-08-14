7d3e1b1321a540b14517e8cd698f4b2d39a02405
pratheesh-TMRC-Nvidia <pratheesh.prakash@mobilerobot.com>
2025-08-14 05:51:57 +0000

    Rslidar - Tmrc launch works and publishes IMU and Lidar data

--- launch/tmrc.launch.py
+++ launch/tmrc.launch.py
@@ -1,10 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
-    config_file = '' # your config file path
+    config_file = os.path.join(
+        get_package_share_directory('rslidar_sdk'),
+        'config',
+        'config.yaml'
+    )
 
     return LaunchDescription([
         Node(
@@ -13,6 +18,6 @@ def generate_launch_description():
             executable='rslidar_sdk_node', 
             output='screen', 
             parameters=[{'config_path': config_file}],
-            remappings=[('/rslidar_imu_data ','imu/data'),
-                        ('/rslidar_points ', 'scan/lidar_3d')],)
+            remappings=[('/rslidar_imu_data', 'imu/data'),
+                        ('/rslidar_points', 'scan/lidar_3d')])
     ])
